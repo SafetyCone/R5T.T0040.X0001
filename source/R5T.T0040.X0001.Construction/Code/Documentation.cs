a856d8a0f20020dc1850eeb37a4b917cc6370300
David Coats <david@getsafetycone.com>
2021-09-21 16:14:27 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0040.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0040.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for Rivet-specific, public, extensions to project and solution path related extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
