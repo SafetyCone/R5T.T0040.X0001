fcbadc3ef3933765eb0a8dd7d84873f72e63a6fc
David Coats <david@getsafetycone.com>
2022-01-05 12:34:21 -0800

    Add solution path operator extensions.

---
+++ source/R5T.T0040.X0001/Code/Bases/Extensions/ISolutionPathsOperatorExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+using R5T.T0040;
+
+using Instances = R5T.T0040.X0001.Instances;
+
+
+namespace System
+{
+    public static class ISolutionPathsOperatorExtensions
+    {
+        public static string GetSolutionDirectoryName(this ISolutionPathsOperator _,
+            string solutionName)
+        {
+            Instances.PathOperator.VerifyNoInvalidPathCharacters(solutionName);
+
+            // After checking, just use the project name as the project directory name.
+            var output = solutionName;
+            return output;
+        }
+
+        public static string GetSolutionDirectoryPathFromSolutionName(this ISolutionPathsOperator _,
+            string parentDirectoryPath,
+            string solutionName)
+        {
+            var solutionDirectoryName = _.GetSolutionDirectoryName(solutionName);
+
+            var output = Instances.PathOperator.AppendDirectoryRelativePathToDirectoryPath(parentDirectoryPath, solutionDirectoryName);
+            return output;
+        }
+
+        public static string GetSourceSolutionDirectoryPath(this ISolutionPathsOperator _,
+            string parentDirectoryPath)
+        {
+            var output = Instances.PathOperator.AppendDirectoryRelativePathToDirectoryPath(
+                parentDirectoryPath,
+                Instances.CodeDirectoryName.Source());
+
+            return output;
+        }
+
+        public static string GetSourceSolutionFilePath(this ISolutionPathsOperator _,
+            string parentDirectoryPath,
+            string solutionName)
+        {
+            var solutionDirectoryPath = _.GetSourceSolutionDirectoryPath(parentDirectoryPath);
+
+            var solutionFileName = Instances.SolutionFileNameOperator.GetSolutionFileName(solutionName);
+
+            var solutionFilePath = Instances.PathOperator.GetFilePath(solutionDirectoryPath, solutionFileName);
+            return solutionFilePath;
+        }
+
+        public static string GetSolutionDirectoryPath(this ISolutionPathsOperator _,
+            string solutionFilePath)
+        {
+            var output = Instances.PathOperator.GetDirectoryPathOfFilePath(solutionFilePath);
+            return output;
+        }
+
+        public static string GetSolutionFilePath(this ISolutionPathsOperator _,
+            string solutionDirectoryPath,
+            string solutionName)
+        {
+            var solutionFileName = Instances.SolutionFileNameOperator.GetSolutionFileName(solutionName);
+
+            var output = Instances.PathOperator.GetFilePath(solutionDirectoryPath, solutionFileName);
+            return output;
+        }
+
+        public static string GetSolutionFilePathForProject(this ISolutionPathsOperator _,
+            string projectFilePath)
+        {
+            var solutionFileExtension = Instances.FileExtension.Sln();
+
+            var path = projectFilePath;
+
+            while (Instances.PathOperator.HasParentDirectory(path))
+            {
+                path = Instances.PathOperator.GetParentDirectoryPath(path);
+
+                var hasSolutionFile = Instances.FileSystemOperator.HasFileInDirectoryByFileExtension(path, solutionFileExtension);
+                if (hasSolutionFile)
+                {
+                    return hasSolutionFile.Result;
+                }
+            }
+
+            // Otherwise, throw.
+            throw new ArgumentException($"No solution file found in parent directories for project file:\n{projectFilePath}", nameof(projectFilePath));
+        }
+
+        public static string GetSolutionName(this ISolutionPathsOperator _,
+            string solutionFilePath)
+        {
+            var solutionFileName = Instances.PathOperator.GetFileNameForFilePath(solutionFilePath);
+
+            var output = Instances.SolutionFileNameOperator.GetSolutionNameFromSolutionFileName(solutionFileName);
+            return output;
+        }
+    }
+}
--- source/R5T.T0040.X0001/Code/Instances.cs
+++ source/R5T.T0040.X0001/Code/Instances.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 
 using R5T.T0021;
+using R5T.T0032;
 using R5T.T0037;
 using R5T.T0041;
+using R5T.T0044;
 
 
 namespace R5T.T0040.X0001
@@ -11,7 +13,10 @@ namespace R5T.T0040.X0001
     {
         public static ICodeDirectoryName CodeDirectoryName { get; } = T0037.CodeDirectoryName.Instance;
         public static ICodeFileName CodeFileName { get; } = T0037.CodeFileName.Instance;
+        public static IFileExtension FileExtension { get; } = T0032.FileExtension.Instance;
         public static IFileName FileName { get; } = T0021.FileName.Instance;
+        public static IFileSystemOperator FileSystemOperator { get; } = T0044.FileSystemOperator.Instance;
         public static IPathOperator PathOperator { get; } = T0041.PathOperator.Instance;
+        public static ISolutionFileNameOperator SolutionFileNameOperator { get; } = T0040.SolutionFileNameOperator.Instance;
     }
 }
