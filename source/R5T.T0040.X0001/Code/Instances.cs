9fc5455fc8873fa9d1f32cc67f276645b897e34f
davidcoats <david@getsafetycone.com>
2022-10-10 22:29:12 -0700

    Interim changes.

--- source/R5T.T0040.X0001/Code/Instances.cs
+++ source/R5T.T0040.X0001/Code/Instances.cs
@@ -15,7 +15,7 @@ namespace R5T.T0040.X0001
         public static ICodeFileName CodeFileName { get; } = T0037.CodeFileName.Instance;
         public static IFileExtension FileExtension { get; } = T0032.FileExtension.Instance;
         public static IFileName FileName { get; } = T0021.FileName.Instance;
-        public static IFileSystemOperator FileSystemOperator { get; } = T0044.FileSystemOperator.Instance;
+        public static T0044.IFileSystemOperator FileSystemOperator { get; } = T0044.FileSystemOperator.Instance;
         public static IPathOperator PathOperator { get; } = F0002.PathOperator.Instance;
         public static ISolutionFileNameOperator SolutionFileNameOperator { get; } = T0040.SolutionFileNameOperator.Instance;
     }
